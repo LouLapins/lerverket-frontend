f2e6d6601e2b448cd356517995af882309328fbd
LouLapins <loulapins@hotmail.com>
2021-12-16 21:08:43 +0100

    create hook to fetch the data from strapi

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string) => {
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    
+    useEffect(()=> {
+    //writing another function inside so i can make it async
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch(url);
+                const json = await res.json();
+                setData(json);
+                setLoading(false);
+
+            } catch (error: any) {
+                setError(error);
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [url])
+
+    return {
+        loading, error, data
+    }
+}
+
+export default useFetch;
