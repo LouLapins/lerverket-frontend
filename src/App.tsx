21d947a18fc256b710552eb65ee0d737681aae68
LouLapins <loulapins@hotmail.com>
2021-12-16 20:32:19 +0100

    add some pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Artworks from './pages/Artworks';
+import Contacts from './pages/Contacts';
+import Courses from './pages/Courses';
+import Landing from './pages/Landing';
+import News from './pages/News';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/courses" element={<Courses/>}/>
+          <Route path="/news" element={<News/>}/>
+          <Route path="/contacts" element={<Contacts/>}/>
+          <Route path="/artworks" element={<Artworks/>}/>
+        </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <Link to="/"><h1>Home</h1></Link>
+        </div>
+    )
+}
