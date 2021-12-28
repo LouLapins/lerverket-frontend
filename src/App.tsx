0759ad3389546afd065c7993bf440ed37b0eec84
LouLapins <loulapins@hotmail.com>
2021-12-28 22:01:21 +0100

    add apollo client to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+//page and layout imports
 import Header from './components/Header';
 import About from './pages/About';
 import ArtworkDetails from './pages/ArtworkDetails';
@@ -8,21 +11,28 @@ import Courses from './pages/Courses';
 import Landing from './pages/Landing';
 import Directions from './pages/Directions';
 
+//apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Header/>
-        <Routes>
-          <Route path="/" element={<Landing/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/courses" element={<Courses/>}/>
-          <Route path="/directions" element={<Directions/>}/>
-          <Route path="/artworks" element={<Artworks/>}/>
-          <Route path="/artworks/details/:id" element={<ArtworkDetails/>}/>
-        </Routes>
-    </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Landing/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/courses" element={<Courses/>}/>
+            <Route path="/directions" element={<Directions/>}/>
+            <Route path="/artworks" element={<Artworks/>}/>
+            <Route path="/artworks/details/:id" element={<ArtworkDetails/>}/>
+          </Routes>
+        </div>
+      </ApolloProvider>
     </Router>
   );
 }
