eb41c12f0b5b1bf6961bc2deb0c90633d18c5b0c
LouLapins <loulapins@hotmail.com>
2022-01-06 15:59:33 +0100

    import and add icons

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { RiInstagramFill, RiFacebookCircleFill } from "react-icons/ri";
 
 export default function Landing() {
     return (
@@ -9,8 +10,8 @@ export default function Landing() {
             <Link to="/contact">Hitta hit</Link>
         </div>
         <div>
-            <Link to="/instagram">Instagram</Link>
-            <Link to="/facebook">Facebook</Link>
+            <Link to="/instagram"><RiInstagramFill/></Link>
+            <Link to="/facebook"><RiFacebookCircleFill/></Link>
         </div>
         </>
     )
